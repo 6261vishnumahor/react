8c42d4edd6674264e12e096138f08e170b26a87b
6261vishnumho <7225vishnumahor@gmail.com>
2025-06-17 21:51:50 +0530

    add files

--- first_react/third_app/src/about.jsx
+++ first_react/third_app/src/about.jsx
@@ -1,16 +1,50 @@
 
-const About=()=>{
-    const arr=[63,45,45,67,4,6,76]
-    return(
-        <>
-        <h1>this is about page</h1>
+const About = () => {
+    const arr = [63, 45, 45, 67, 4, 6, 76]
+
+    const dd=[{
+        "name":"vishnu",
+        "email":"7225vishnumahor@gmail.com"
+    }]
+
+    const dev = [
         {
-            arr.map(e=>(
-                <div>
-                    <div>{e}</div>
-                </div>
-            ))
+            "producted": "shoes",
+            "productprice": 3000,
+            "discount": "30%",
+            "image": <img width="200px" src="https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg" ></img>
         }
+    ]
+    return (
+        <>
+            <h1>this is about page</h1>
+            {
+                arr.map(e => (
+
+                    <div>
+                        <div>{e}</div>
+                    </div>
+
+                ))
+            }
+            {
+                dd.map(a => (
+                    <div>
+                        <div>{a.name}</div>
+                        <div>{a.email}</div>
+                    </div>
+                ))
+            }
+            {
+                dev.map(b => (
+                    <div>
+                        <div>{b.producted}</div>
+                        <div>{b.productprice}</div>
+                        <div>{b.image}</div>
+                        <div>{b.discount}</div>
+                    </div>
+                ))
+            }
         </>
     )
 }
--- first_react/third_app/src/home.jsx
+++ first_react/third_app/src/home.jsx
@@ -1,7 +1,32 @@
-const Home=()=>{
-    return(
+import { useRef } from "react";
+
+const Home = () => {
+    const ref = useRef();
+    const ref1 = useRef();
+    const c = () => {
+        ref.current.src = "https://m.media-amazon.com/images/I/61n0aVXta7L._UY1000_.jpg";
+
+    }
+    const c1 = () => {
+        ref.current.src = "https://www.sonatawatches.in/dw/image/v2/BKDD_PRD/on/demandware.static/-/Sites-titan-master-catalog/default/dw7111545d/images/Sonata/Catalog/77105SL11_1.jpg?sw=600&sh=600"
+    }
+    const c2 = () => {
+        ref1.current.textContent = "this is vishnu page"
+    }
+     const c3 = () => {
+        ref1.current.textContent = "sandhya rajput khandawa"
+    }
+
+    return (
         <>
-        <h1> this is home page </h1>
+            {/* <div onMouseEnter={c2} ref={ref1}> this is home page </div> */}
+
+
+            <div onMouseEnter={c} onMouseLeave={c1} width="200px" height="200px" ><img ref={ref} width="200px" height="200px" src="https://www.sonatawatches.in/dw/image/v2/BKDD_PRD/on/demandware.static/-/Sites-titan-master-catalog/default/dw7111545d/images/Sonata/Catalog/77105SL11_1.jpg?sw=600&sh=600" alt="" /></div>
+            <div onMouseEnter={c2} onMouseLeave={c3} ref={ref1}> bifjsdklcoapksld;fmejkdslkoeplds</div>
+
+
+
         </>
     )
 }
